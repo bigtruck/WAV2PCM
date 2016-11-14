b0cdebe5af5f0fc2589542b5d2d80669b6d45be1
BigTruck <xiaoyong_dev@163.com>
2016-11-14 17:29:25 +0800

    添加项目文件。

---
+++ WAV2PCM/WAV2PCMDlg.h
@@ -0,0 +1,34 @@
+
+// WAV2PCMDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CWAV2PCMDlg 对话框
+class CWAV2PCMDlg : public CDialogEx
+{
+// 构造
+public:
+	CWAV2PCMDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_WAV2PCM_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
